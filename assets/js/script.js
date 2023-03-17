3b2d0885136557c1cffb8dca91440a5b64e0c295
Josiel Nascimento <jsilverionascimento@gmail.com>
2023-03-16 22:11:54 -0300

    pagina index e animacoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,23 @@ setTimeout(() => {
 window.addEventListener('beforeunload', function() {
   pagina.classList.add('saindo');
 });
+
+
+function adicionaClasseAoRolar() {
+    // seleciona a div
+    const minhaDiv = document.querySelector('.doutor');
+  
+    // determina o ponto de rolagem para adicionar a classe
+    const pontoDeRolagem = 700; // ajuste o valor conforme necessário
+  
+    // adiciona a classe "ativo" quando a página é rolada para o ponto
+    window.addEventListener('scroll', function() {
+      if (window.pageYOffset >= pontoDeRolagem) {
+        minhaDiv.classList.add('doutorAtivo');
+      } 
+    });
+  }
+  
+  // chama a função quando a página é carregada
+  window.addEventListener('load', adicionaClasseAoRolar);
+  
