2fc68f27cc6cdf84a7ab465d852be489ff5fa676
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2023-05-24 21:28:28 -0300

    Tela de Cadastro Concluída e Funcional

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const passwordCheck = document.getElementById('passwordCheck');
 const date = document.getElementById('date');
 const rg = document.getElementById('RG');
 const cpf = document.getElementById('CPF');
+const responsavel = document.getElementById('responsavel');
+const RGresponsavel = document.getElementById('RGResponsavel');
+const CPFresponsavel = document.getElementById('CPFResponsavel');
+const phone = document.getElementById('phone');
+const cep = document.getElementById('CEP');
+const adress = document.getElementById('Endereco');
+const number = document.getElementById('Numero');
+const complement = document.getElementById('Complemento');
+const emailCheck = document.getElementById('emailCheck');
 const dependente = document.querySelectorAll('input[name="dependente"]');
-let formControls = [userName, email, password, passwordCheck, date, rg, cpf];
+let formControls = [userName, email, password, passwordCheck, date, rg, cpf, responsavel];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     checkInputs();
 });
 
@@ -23,25 +32,42 @@ function checkInputs() {
     const dateValue = date.value.trim();
     const rgValue = rg.value.trim();
     const cpfValue = cpf.value.trim();
-    let dependenteValue='';
+    const responsavelValue = responsavel.value.trim();
+    const RGresponsavelValue = RGresponsavel.value.trim();
+    const CPFresponsavelValue = CPFresponsavel.value.trim();
+    const phoneValue = phone.value.trim();
+    const cepValue = cep.value.trim();
+    const adressValue = adress.value.trim();
+    const numberValue = number.value.trim();
+    const complementValue = complement.value.trim();
+    const emailCheckValue = emailCheck.value.trim();
+    let dependenteValue = '';
+    const dependenteArray = Array.from(dependente);
 
-    dependente.forEach((radio) => {
+    dependenteArray.forEach((radio) => {
         if (radio.checked) {
-          dependenteValue = radio.value;
+            dependenteValue = radio.value.trim();
         }
-      });
-    console.log(dependenteValue)
+    });
+    console.log(dependenteValue);
+
+    if (dependenteValue === '') {
+        errorValidation(dependente[0], 'Preencha esse campo');
+
+    } else {
+        successValidation(dependente[0]);
+    };
 
     if (nameValue === '') {
         errorValidation(userName, 'Preencha esse campo');
-        
+
     } else {
         successValidation(userName);
     };
 
     if (emailValue === '') {
         errorValidation(email, 'Preencha esse campo');
-        
+
     } else {
         successValidation(email);
     };
@@ -49,14 +75,14 @@ function checkInputs() {
     if (passwordValue === '') {
         errorValidation(password, 'Preencha esse campo');
     } else if (passwordValue.length < 8) {
-        errorValidation(password, 'A senha deve ter 8 ou mais caracteres');  
-    }else {
+        errorValidation(password, 'A senha deve ter 8 ou mais caracteres');
+    } else {
         successValidation(password);
     };
 
     if (passwordCheckValue === '') {
         errorValidation(passwordCheck, 'Preencha esse campo');
-        
+
     } else if (passwordCheckValue !== passwordValue) {
         errorValidation(passwordCheck, 'As senhas são diferentes');
     } else {
@@ -65,23 +91,87 @@ function checkInputs() {
 
     if (dateValue === '') {
         errorValidation(date, 'Preencha esse campo');
-        
+
     } else {
         successValidation(date);
     };
 
     if (rgValue === '') {
         errorValidation(rg, 'Preencha esse campo');
-        
+
     } else {
         successValidation(rg);
     };
 
     if (cpfValue === '') {
         errorValidation(cpf, 'Preencha esse campo');
-        
+
     } else {
         successValidation(cpf);
+    };
+
+    if (responsavelValue === '') {
+        errorValidation(responsavel, 'Preencha esse campo');
+
+    } else {
+        successValidation(responsavel);
+    };
+
+    if (RGresponsavelValue === '') {
+        errorValidation(RGresponsavel, 'Preencha esse campo');
+
+    } else {
+        successValidation(RGresponsavel);
+    };
+
+    if (CPFresponsavelValue === '') {
+        errorValidation(CPFresponsavel, 'Preencha esse campo');
+
+    } else {
+        successValidation(CPFresponsavel);
+    };
+
+    if (phoneValue === '') {
+        errorValidation(phone, 'Preencha esse campo');
+
+    } else {
+        successValidation(phone);
+    };
+
+    if (cepValue === '') {
+        errorValidation(cep, 'Preencha esse campo');
+
+    } else {
+        successValidation(cep);
+    };
+
+    if (adressValue === '') {
+        errorValidation(adress, 'Preencha esse campo');
+
+    } else {
+        successValidation(adress);
+    };
+
+    if (numberValue === '') {
+        errorValidation(number, 'Preencha esse campo');
+
+    } else {
+        successValidation(number);
+    };
+
+    if (complementValue === '') {
+        errorValidation(complement, 'Preencha esse campo');
+
+    } else {
+        successValidation(complement);
+    };
+
+    if (emailCheckValue === '') {
+        errorValidation(emailCheck, 'Preencha esse campo');
+    } else if (emailCheckValue !== emailValue) {
+        errorValidation(emailCheck, 'Os e-mails são diferentes');
+    } else {
+        successValidation(emailCheck);
     };
 };
 
@@ -95,6 +185,30 @@ function errorValidation(input, message) {
 
     const messageError = formControl.querySelector('small');
     messageError.innerHTML = message;
+
+    if (input instanceof NodeList) {
+        // Para cada elemento da NodeList, adiciona a classe 'error' ao elemento pai
+        // e exibe a mensagem de erro correspondente
+        input.forEach((item) => {
+            const formControl = item.parentElement;
+            formControl.classList.add('error');
+            formControl.classList.remove('success');
+
+            shake(formControl);
+
+            const messageError = formControl.querySelector('small');
+            messageError.innerHTML = message;
+        });
+    } else {
+        const formControl = input.parentElement;
+        formControl.classList.add('error');
+        formControl.classList.remove('success');
+
+        shake(formControl);
+
+        const messageError = formControl.querySelector('small');
+        messageError.innerHTML = message;
+    }
 };
 
 function successValidation(input) {
@@ -103,9 +217,9 @@ function successValidation(input) {
     formControl.classList.remove('error');
 };
 
-function shake (formControl){
+function shake(formControl) {
     formControl.classList.add('shake');
-    formControl.addEventListener('animationend', ()=>{
+    formControl.addEventListener('animationend', () => {
         formControl.classList.remove('shake');
     });
 };
