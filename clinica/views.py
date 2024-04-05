9df414ee119a018509744c75859294b6eee74beb
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2024-04-04 22:24:23 -0300

    Fim do formulário de contato

--- clinica/views.py
+++ clinica/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, 'index.html')
 
 def contact_us(request):
     if request.POST:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        assunto = request.POST.get('assunto')
+        mensagem = request.POST.get('mensagem')
+        send_mail(
+            f'Mensagem de {nome}: {assunto}',
+            f'Nome: {nome} \nEmail:{email} \nAssunto:{assunto} \n{mensagem}',
+            "avelinogabrieldossantos@gmail.com",
+            ["aluno103.23187221gabriel@gmail.com"],
+            fail_silently=False,
+        )
         return redirect(home)
     else:
         return render(request, 'contact-forms.html')
