665d6c970d82267567eb2af8129f4cc04a6b9bba
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2024-04-10 22:00:49 -0300

    Separação dos templates em apps

--- clinica_salvino/settings.py
+++ clinica_salvino/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'clinica',
-    'admin',
+    'recept',
     'medico',
     'paciente',
     'proprietario'
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
