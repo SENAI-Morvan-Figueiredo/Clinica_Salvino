edd7f3712494e21ed146fa7761c7d94ce0fb6a5f
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-18 12:17:56 -0300

    Views paciente: limpeza de código

--- paciente/views.py
+++ paciente/views.py
@@ -4,7 +4,6 @@ from .forms import CadPaciente
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Paciente
 
 logger = logging.getLogger(__name__)
 
