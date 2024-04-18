493b864078535d425881d8419ea59107d2257dd7
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-18 15:50:46 -0300

    Tela de recepcionista

--- recept/views.py
+++ recept/views.py
@@ -4,4 +4,5 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def receptBoard(request):
-    return render(request, 'recepcionista.html')
+    recepcionista = request.user.recepcionista
+    return render(request, 'recepcionista.html', {'recepcionista': recepcionista})
