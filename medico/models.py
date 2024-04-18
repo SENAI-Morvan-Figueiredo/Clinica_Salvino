6d155f6cd1a1b7c03a490e768a30f0e4ebe61031
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-18 16:57:13 -0300

    Criação Model Especialidade

--- medico/models.py
+++ medico/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Especialidade(models.Model):
+    nome_especialidade = models.CharField(max_length=256)
 
 class Medico(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
