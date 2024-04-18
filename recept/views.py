138e38e0a3fcccde9005093da605ccacab8dc1cd
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-18 12:25:37 -0300

    Renomeando dashboard de admin para recepcionista

--- recept/views.py
+++ recept/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def receptBoard(request):
-    return render(request, 'admin.html')
+    return render(request, 'recepcionista.html')
