2c8062181b43b46941112f98e363b7bdf66c712f
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-18 16:30:03 -0300

    Models de convênio e Tratamento

--- clinica/admin.py
+++ clinica/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Convenio, Tratamento
 
 # Register your models here.
+admin.site.register(Convenio)
+admin.site.register(Tratamento)
+
--- clinica/models.py
+++ clinica/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Convenio(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+class Tratamento(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    descricao = models.TextField()
