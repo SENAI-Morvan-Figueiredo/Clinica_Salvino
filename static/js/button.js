5e1dcc5a0436aef0a7449077353b869264bf0499
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-04-30 17:00:15 -0300

    Adição de botão volta

--- static/js/button.js
+++ static/js/button.js
@@ -1,4 +1,11 @@
+const forms = document.querySelectorAll('form')
+
 function voltar(){
+    forms.forEach((form) =>{
+        form.addEventListener('submit', (event)=>{
+            event.preventDefault()
+        })
+    })
     history.back();
 }
 
