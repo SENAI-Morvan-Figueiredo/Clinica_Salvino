f34570ac574f17bfc33469b7aa9bc7b8db285bb6
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-05-02 09:31:32 -0300

    Mudança nos botões

--- clinica/views.py
+++ clinica/views.py
@@ -64,13 +64,13 @@ def login(request):
                 if user:
                     Login_django(request, user)
                     if Paciente.objects.filter(user=user).exists():
-                        return redirect('paciente')
+                        return redirect('paciente_dash')
                     elif Proprietario.objects.filter(user=user).exists():
-                        return redirect('proprietario')
+                        return redirect('proprietario_dash')
                     elif Medico.objects.filter(user=user).exists():
-                        return redirect('medico')
+                        return redirect('medico_dash')
                     elif Recepcionista.objects.filter(user=user).exists():
-                        return redirect('recepcionista')
+                        return redirect('recepcionista_dash')
                 else:
                     messages.error(request, "Credenciais inválidas. Por favor, verifique seu email e senha.")
                     return redirect('login')
