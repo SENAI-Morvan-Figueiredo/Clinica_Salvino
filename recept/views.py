9c049b4c574a66d57cc331664b771b79ef5d1291
diegoleite27 <alumo103.23187386diego@gmail.com>
2024-05-09 21:55:52 -0300

    atualização da views

--- recept/views.py
+++ recept/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 
-# Create your views here.
+
 @login_required
 def receptBoard(request):
     recepcionista = request.user.recepcionista
