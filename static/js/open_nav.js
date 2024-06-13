924363afff13a153bd81e6e1fc49c5fdbc388cdc
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-06-13 07:33:57 -0300

    Sistema de menu responsivo com classes corrigido 2

--- static/js/open_nav.js
+++ static/js/open_nav.js
@@ -1,37 +1,40 @@
-const btn_nav = document.querySelector('.btn-nav')
-const background = document.querySelector('.nav_menu')
-const nav_menu = document.querySelector('.dash_menu_user')
-const close_button = document.querySelector('.close_button')
+document.addEventListener('DOMContentLoaded', function () {
+    const btn_nav = document.querySelector('.btn-nav');
+    const background = document.querySelector('.nav_menu');
+    const nav_menu = document.querySelector('.dash_menu_user');
+    const close_button = document.querySelector('.close_button');
 
-btn_nav.addEventListener('click', ()=>{
-    background.classList.add('ativo');
-    nav_menu.classList.add('ativo');
-    close_button.classList.add('ativo');
-});
-
-close_button.addEventListener('click', ()=>{
-    background.classList.remove('ativo');
-    nav_menu.classList.remove('ativo');
-    close_button.classList.remove('ativo');
-});
-
-function resetStylesOnHeightChange() {
-    if (window.innerWidth > 880) {
-        background.classList.remove('ativo');
+    btn_nav.addEventListener('click', () => {
+        background.classList.add('ativo');
         nav_menu.classList.add('ativo');
-        close_button.classList.remove('ativo');
-    } else {
+        close_button.classList.add('ativo');
+    });
+
+    close_button.addEventListener('click', () => {
         background.classList.remove('ativo');
         nav_menu.classList.remove('ativo');
         close_button.classList.remove('ativo');
+    });
+
+    function resetStylesOnResize() {
+        if (window.innerWidth > 880) {
+            background.classList.remove('ativo');
+            nav_menu.classList.add('ativo');
+            close_button.classList.remove('ativo');
+        } else {
+            background.classList.remove('ativo');
+            nav_menu.classList.remove('ativo');
+            close_button.classList.remove('ativo');
+        }
     }
-}
 
-window.addEventListener('resize', resetStylesOnHeightChange);
+    window.addEventListener('resize', resetStylesOnResize);
 
-// Chama a função para garantir o estado inicial correto
-resetStylesOnHeightChange();
+    // Chama a função para garantir o estado inicial correto
+    resetStylesOnResize();
 
-nav_menu.addEventListener('touchmove', function (event) {
-    event.stopPropagation();
-}, false);
+    // Previne o fechamento ao scroll dentro do menu
+    nav_menu.addEventListener('touchmove', function (event) {
+        event.stopPropagation();
+    }, false);
+});
