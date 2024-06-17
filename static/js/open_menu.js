9224b0ff221beaa5ee8c85ae1398a87f28392fb2
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-06-17 08:11:37 -0300

    Correção de lista 4

--- static/js/open_menu.js
+++ static/js/open_menu.js
@@ -3,26 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
     // Pega todos os botões que têm a classe 'open-btn'
     const openButtons = document.querySelectorAll('.open-btn');
 
+    let openMenu = null; // Variável para rastrear o menu aberto
+
     const closeAllMenus = () => {
         const allMenus = document.querySelectorAll('.menu');
         allMenus.forEach(menu => {
             menu.style.display = 'none';
         });
+        openMenu = null; // Reseta a variável de menu aberto
     };
 
     openButtons.forEach(button => {
         button.addEventListener('click', function(event) {
             event.preventDefault(); // Previne comportamento padrão do link (caso tenha um href='#')
 
-            closeAllMenus();
             // Encontra o menu relacionado a este botão
             const menu = this.nextElementSibling.nextElementSibling;
 
-            // Alterna a visibilidade do menu
-            if (menu.style.display === 'none' || menu.style.display === '') {
-                menu.style.display = 'block';
-            } else {
+            // Verifica se este menu já está aberto
+            if (menu === openMenu) {
+                // Fecha o menu se já estiver aberto
                 menu.style.display = 'none';
+                openMenu = null; // Reseta a variável de menu aberto
+            } else {
+                // Fecha todos os menus antes de abrir o menu desejado
+                closeAllMenus();
+                // Abre o menu relacionado ao botão clicado
+                menu.style.display = 'block';
+                openMenu = menu; // Atualiza a variável de menu aberto
             }
         });
     });
